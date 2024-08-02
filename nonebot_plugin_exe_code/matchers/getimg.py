183359b231d2557a2ce4d385d0dee28f27cdb785
wyf7685 <wyf7685@163.com>
2024-08-02 13:49:32 +0800

    使用 UniMessage 处理消息发送

--- nonebot_plugin_exe_code/matchers/getimg.py
+++ nonebot_plugin_exe_code/matchers/getimg.py
@@ -3,8 +3,7 @@ from io import BytesIO
 import PIL.Image
 from nonebot import on_startswith
 from nonebot.adapters import Bot, Event
-from nonebot.matcher import Matcher
-from nonebot_plugin_alconna.uniseg import image_fetch
+from nonebot_plugin_alconna.uniseg import image_fetch, UniMessage
 
 from .depends import EXECODE_ENABLED, CodeContext, EventImage
 
@@ -16,20 +15,19 @@ async def handle_getimg(
     bot: Bot,
     event: Event,
     ctx: CodeContext,
-    matcher: Matcher,
     image: EventImage,
 ):
     varname = event.get_message().extract_plain_text().removeprefix("getimg").strip()
     if (varname := varname or "img") and not varname.isidentifier():
-        await matcher.finish(f"{varname} 不是一个合法的 Python 标识符")
+        await UniMessage(f"{varname} 不是一个合法的 Python 标识符").finish()
 
     try:
         img_bytes = await image_fetch(event, bot, {}, image)
         if not isinstance(img_bytes, bytes):
             raise ValueError(f"获取图片数据类型错误: {type(img_bytes)!r}")
     except Exception as err:
-        await matcher.finish(f"保存图片时出错: {err}")
+        await UniMessage(f"保存图片时出错: {err}").finish()
 
     ctx.set_gurl(image)
     ctx.set_value(varname, PIL.Image.open(BytesIO(img_bytes)))
-    await matcher.finish(f"图片已保存至变量 {varname}")
+    await UniMessage(f"图片已保存至变量 {varname}").finish()
