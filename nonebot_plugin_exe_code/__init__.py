37b94d79476fbfbcdb1c2214b31b8e4c68f03f19
wyf7685 <wyf7685@163.com>
2024-08-07 22:04:32 +0800

    修改插件依赖声明

--- nonebot_plugin_exe_code/__init__.py
+++ nonebot_plugin_exe_code/__init__.py
@@ -2,14 +2,11 @@ from nonebot import require
 from nonebot.plugin import PluginMetadata
 from nonebot.plugin.load import inherit_supported_adapters
 
-requirements = {
-    "nonebot_plugin_alconna",
-    "nonebot_plugin_localstore",
-    "nonebot_plugin_session",
-    "nonebot_plugin_userinfo",
-    "nonebot_plugin_waiter",
-}
-[require(i) for i in requirements]
+require("nonebot_plugin_alconna")
+require("nonebot_plugin_localstore")
+require("nonebot_plugin_session")
+require("nonebot_plugin_userinfo")
+require("nonebot_plugin_waiter")
 
 from . import matchers
 from .config import Config
@@ -22,7 +19,12 @@ __plugin_meta__ = PluginMetadata(
     type="application",
     homepage="https://github.com/wyf7685/nonebot-plugin-exe-code",
     config=Config,
-    supported_adapters=inherit_supported_adapters(*requirements),
+    supported_adapters=inherit_supported_adapters(
+        "nonebot_plugin_alconna",
+        "nonebot_plugin_session",
+        "nonebot_plugin_userinfo",
+        "nonebot_plugin_waiter",
+    ),
     extra={
         "author": "wyf7685",
         "version": __version__,
