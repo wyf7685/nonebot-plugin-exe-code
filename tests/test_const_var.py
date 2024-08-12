7d72afd8053062ec8e4dcad16c2d67e9232b6ea6
wyf7685 <wyf7685@163.com>
2024-08-12 18:53:30 +0800

    添加 const_var 测试样例

--- tests/test_const_var.py
+++ tests/test_const_var.py
@@ -15,6 +15,10 @@ api.set_const("test_var")
 reset()
 """
 
+code_test_invalid_const_var = """\
+api.set_const('@@@', 123)
+"""
+
 
 @pytest.mark.asyncio()
 async def test_const_var(app: App):
@@ -51,3 +55,29 @@ async def test_const_var(app: App):
 
     context = Context.get_context(str(user_id)).ctx
     assert context.get("test_var") is None
+
+
+@pytest.mark.asyncio()
+async def test_invalid_const_var(app: App):
+    from nonebot_plugin_exe_code.matchers.code import matcher
+
+    async with app.test_matcher(matcher) as ctx:
+        adapter = ctx.create_adapter(base=Adapter)
+        bot = ctx.create_bot(base=Bot, adapter=adapter)
+        bot.adapter.__class__.get_name = Adapter.get_name
+
+        user_id = fake_user_id()
+        event = fake_group_exe_code(
+            exe_code_group,
+            user_id,
+            code_test_invalid_const_var,
+        )
+        expected = Message("""执行失败: ValueError("'@@@' 不是合法的 Python 标识符")""")
+        ctx.receive_event(bot, event)
+        ctx.should_pass_permission(matcher)
+        ctx.should_call_api(
+            "get_group_member_info",
+            {"group_id": exe_code_group, "user_id": user_id},
+            {"user_id": user_id, "sex": "unkown", "card": "", "nickname": ""},
+        )
+        ctx.should_call_send(event, expected)
