ca4e5ee7a202a3995de4e381e543741753de9835
wyf7685 <wyf7685@163.com>
2024-09-08 11:23:47 +0800

    satori 接口: 群禁言

--- nonebot_plugin_exe_code/interface/adapter_api/satori.py
+++ nonebot_plugin_exe_code/interface/adapter_api/satori.py
@@ -1,11 +1,37 @@
 import contextlib
+from typing import cast
 
 from ..api import API as BaseAPI
 from ..api import register_api
+from ..help_doc import descript
+from ..utils import debug_log
 
 with contextlib.suppress(ImportError):
-    from nonebot.adapters.satori import Adapter
+    from nonebot.adapters.satori import Adapter, Bot
 
     @register_api(Adapter)
     class API(BaseAPI):
-        pass
+        @descript(
+            description="设置群禁言",
+            parameters=dict(
+                duration="禁言时间, 单位秒, 填0为解除禁言",
+                qid="被禁言者ID",
+                gid="所在群组ID, 默认为当前群聊, 私聊时必填",
+            ),
+        )
+        @debug_log
+        async def set_mute(
+            self,
+            duration: float,
+            qid: str | int | None = None,
+            gid: str | int | None = None,
+        ) -> None:
+            if gid is None:
+                gid = self.gid
+            if (gid := str(gid)).startswith("private:"):
+                raise ValueError("未指定群组ID")
+            await cast(Bot, self.bot).guild_member_mute(
+                guild_id=str(gid),
+                user_id=str(qid or self.qid),
+                duration=float(duration) * 1000,
+            )
