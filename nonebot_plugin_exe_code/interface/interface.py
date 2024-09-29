c4845ac21bd5b6cacba37100e74d93b74c0eab4a
wyf7685 <wyf7685@163.com>
2024-09-29 17:17:47 +0800

    remove InterfaceMeta

--- nonebot_plugin_exe_code/interface/interface.py
+++ nonebot_plugin_exe_code/interface/interface.py
@@ -3,7 +3,7 @@ from typing import Any, ClassVar, NamedTuple, cast
 
 from typing_extensions import Self
 
-from ..constant import INTERFACE_INST_NAME, INTERFACE_METHOD_DESCRIPTION, T_Context
+from ..constant import INTERFACE_METHOD_DESCRIPTION, T_Context
 from .help_doc import FuncDescription
 from .utils import is_export_method
 
@@ -15,50 +15,10 @@ class _Desc(NamedTuple):
     description: str
 
 
-class InterfaceMeta(type):
-    __export_method__: list[str]
-    __method_description__: dict[str, FuncDescription]
-
-    def __new__(  # noqa: ANN204
-        cls, name: str, bases: tuple[type, ...], attrs: dict[str, Any]
-    ):
-        # default instance name
-        attrs.setdefault(INTERFACE_INST_NAME, name.lower())
-
-        Interface = super().__new__(cls, name, bases, attrs)  # noqa: N806
-        attr = Interface.__dict__  # shortcut
-
-        # export
-        Interface.__export_method__ = [
-            name for name, value in attr.items() if is_export_method(value)
-        ]
-
-        # description
-        Interface.__method_description__ = {
-            name: desc
-            for name, value in attr.items()
-            if (desc := getattr(value, INTERFACE_METHOD_DESCRIPTION, None))
-        }
-
-        return Interface
-
-    def get_export_method(cls) -> Generator[str, None, None]:
-        for c in reversed(cls.mro()):
-            if isinstance(c, InterfaceMeta) and hasattr(c, "__export_method__"):
-                yield from c.__export_method__
-
-    def _get_method_description(cls) -> Generator[_Desc, None, None]:
-        inst_name: str = cls.__inst_name__  # pyright:ignore[reportAttributeAccessIssue]
-        for func_name, desc in cls.__method_description__.items():
-            func = cast(Callable[..., Any], getattr(cls, func_name))
-            is_export = is_export_method(func)
-            yield _Desc(inst_name, func_name, is_export, desc.format(func))
-
-
-class Interface(metaclass=InterfaceMeta):
+class Interface:
     __inst_name__: ClassVar[str] = "interface"
-    __export_method__: ClassVar[list[str]]
-    __method_description__: ClassVar[dict[str, str]]
+    __export_method__: ClassVar[list[str]] = []
+    __method_description__: ClassVar[dict[str, FuncDescription]] = {}
 
     __context: T_Context | None
     __exported: set[str]
@@ -66,6 +26,18 @@ class Interface(metaclass=InterfaceMeta):
     def __init__(self, context: T_Context | None = None) -> None:
         self.__context = context
         self.__exported = set()
+
+    def __init_subclass__(cls) -> None:
+        if cls.__inst_name__ == Interface.__inst_name__:
+            cls.__inst_name__ = cls.__name__.lower()
+        cls.__export_method__ = [
+            name for name, value in cls.__dict__.items() if is_export_method(value)
+        ]
+        cls.__method_description__ = {
+            name: desc
+            for name, value in cls.__dict__.items()
+            if (desc := getattr(value, INTERFACE_METHOD_DESCRIPTION, None))
+        }
 
     def _export(self, key: str, val: Any) -> None:
         if self.__context is None:
@@ -77,7 +49,7 @@ class Interface(metaclass=InterfaceMeta):
 
     def export(self) -> None:
         self._export(self.__inst_name__, self)
-        for name in type(self).get_export_method():
+        for name in self._get_export_method():
             self._export(name, getattr(self, name))
 
     def __enter__(self) -> Self:
@@ -95,6 +67,24 @@ class Interface(metaclass=InterfaceMeta):
 
     async def __aexit__(self, *_: object) -> bool:
         return self.__exit__(*_)
+
+    @classmethod
+    def _get_export_method(cls) -> Generator[str, None, None]:
+        for c in reversed(cls.mro()):
+            if issubclass(c, Interface):
+                yield from c.__export_method__
+
+    @classmethod
+    def _get_method_description(cls) -> Generator[_Desc, None, None]:
+        for func_name, desc in cls.__method_description__.items():
+            func = cast(Callable[..., Any], getattr(cls, func_name))
+            is_export = is_export_method(func)
+            yield _Desc(
+                inst_name=cls.__inst_name__,
+                func_name=func_name,
+                is_export=is_export,
+                description=desc.format(func),
+            )
 
     @classmethod
     def get_all_description(cls) -> tuple[list[str], list[str]]:
