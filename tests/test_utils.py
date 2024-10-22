3c6667762c3ddab3fe11a57de2d014c088ac5586
wyf7685 <wyf7685@163.com>
2024-10-22 14:36:16 +0800

    add test case

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,4 +1,4 @@
-from collections.abc import Awaitable, Callable, Iterable
+from collections.abc import Awaitable, Callable
 from typing import Any
 
 import pytest
@@ -7,89 +7,6 @@ from nonebug import App
 
 from .fake.common import ensure_context
 from .fake.onebot11 import fake_v11_bot, fake_v11_event_session
-
-
-@pytest.mark.usefixtures("app")
-def test_make_wrapper() -> None:
-    from nonebot_plugin_exe_code.interface.decorators import (
-        T_Args,
-        T_Kwargs,
-        make_wrapper,
-    )
-
-    before_called = after_called = False
-
-    def func(*args: Any, **kwargs: Any) -> tuple[T_Args, T_Kwargs]:
-        return args, kwargs
-
-    def before(args: T_Args, kwargs: T_Kwargs) -> tuple[T_Args, T_Kwargs]:
-        nonlocal before_called
-        before_called = True
-        return ((111, *args), {"aaa": 222, **kwargs})
-
-    def after(args: T_Args, kwargs: T_Kwargs, result: Any) -> tuple[bool, Any]:
-        nonlocal after_called
-        after_called = True
-        return True, (args, kwargs, result)
-
-    result = make_wrapper(func, before, after)(333, bbb=444)
-    expected = (
-        (111, 333),
-        {"aaa": 222, "bbb": 444},
-        ((111, 333), {"aaa": 222, "bbb": 444}),
-    )
-
-    assert before_called
-    assert after_called
-    assert result == expected
-
-
-@pytest.mark.usefixtures("app")
-def test_strict() -> None:
-    from nonebot_plugin_exe_code.interface.utils import strict
-
-    with pytest.raises(TypeError, match="not typed"):
-
-        @strict
-        def _test_no_annotation(arg) -> None: ...  # noqa: ANN001
-
-    @strict
-    def _test_1(arg1: Any, arg2: str, arg3: int) -> None: ...
-
-    assert _test_1(..., "123", 456) is None
-    with pytest.raises(TypeError):
-        _test_1(..., 123, "456")  # pyright: ignore[reportArgumentType]
-
-    @strict
-    def _test_2(arg: str | dict | set) -> None: ...
-
-    assert _test_2("string") is None
-    assert _test_2({}) is None
-    assert _test_2({...}) is None
-    with pytest.raises(TypeError):
-        _test_2(arg=123)  # pyright: ignore[reportArgumentType]
-
-    @strict
-    def _test_3(arg: Iterable) -> None: ...
-
-    assert _test_3(["123"]) is None
-    assert _test_3("123") is None
-    with pytest.raises(TypeError):
-        _test_3(123)  # pyright: ignore[reportArgumentType]
-
-    class _Test:
-        @strict
-        def method(self) -> None: ...
-        @classmethod
-        @strict
-        def classmethod_(cls) -> None: ...
-        @staticmethod
-        @strict
-        def staticmethod_() -> None: ...
-
-    assert not hasattr(_Test.method, "__wrapped__")
-    assert not hasattr(_Test.classmethod_, "__wrapped__")
-    assert not hasattr(_Test.staticmethod_, "__wrapped__")
 
 
 @pytest.mark.asyncio
